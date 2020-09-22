26ab075d1752076844e8c303e75729f984a5ebfd
Daniel Ferreira <danielteodoro2010@hotmail.com>
2020-09-22 19:39:02 -0300

    primeira versão

---
+++ PROVAH1/Model/Time.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROVAH1.Model
+{
+    public class Time
+    {
+       
+
+            public int id { get; set; }
+            public string nomeCandidato { get; set; }
+            public string partido { get; set; }
+            public int numero { get; set; }
+            public string cargo { get; set; }
+            public string foto { get; set; }
+
+
+             }
+
+
+}
